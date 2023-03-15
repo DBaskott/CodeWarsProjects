374292fa0fa19d92343a3244b01aa15e415155ef
DBaskott <127939079+DBaskott@users.noreply.github.com>
2023-03-15 14:28:04 +0000

    Create VowelCount.py

---
+++ VowelCount.py
@@ -0,0 +1,9 @@
+def get_count(sentence):
+    output = 0
+    output = output + sentence.count('a')
+    output = output + sentence.count('e')
+    output = output + sentence.count('i')
+    output = output + sentence.count('o')
+    output = output + sentence.count('u')
+    return output 
+    pass
